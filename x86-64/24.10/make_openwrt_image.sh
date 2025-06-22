6026a88b36b0c56a4da4bb921deedbd06b456ff8
marttracg <martt4946@gmail.com>
2025-06-22 18:16:39 +0800

    [DEV]

--- x86-64/24.10/make_openwrt_image.sh
+++ x86-64/24.10/make_openwrt_image.sh
@@ -35,28 +35,34 @@ chmod +x "$UCI_SCRIPT"
 PKG="curl luci-i18n-firewall-zh-tw luci-i18n-package-manager-zh-tw \
      luci-i18n-ttyd-zh-tw script-utils"
 
-EXTRA="base-files ca-bundle dnsmasq dropbear e2fsprogs firewall4 fstools \
+EXTRA1="base-files ca-bundle dnsmasq dropbear e2fsprogs firewall4 fstools \
 grub2-bios-setup kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls \
-logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only opkg partx-utils ppp \
-ppp-mod-pppoe procd-ujail uci uclient-fetch urandom-seed urngd \
+logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only opkg partx-utils \
+procd-ujail uci uclient-fetch urandom-seed urngd \
 kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-dwmac-intel kmod-e1000e kmod-e1000 \
 kmod-forcedeth kmod-fs-vfat kmod-igb kmod-igc kmod-ixgbe kmod-r8169 kmod-tg3 \
-kmod-drm-i915 luci kmod-mtk-t7xx kmod-phy-aquantia kmod-sfp kmod-wwan \
+luci kmod-mtk-t7xx kmod-phy-aquantia kmod-sfp kmod-wwan \
 kmod-usb-net-rndis kmod-usb-serial kmod-usb-serial-option kmod-usb3 \
 luci-proto-mbim luci-proto-ncm comgt pciutils usbutils block-mount \
 ethtool-full iperf3 luci-i18n-base-zh-tw kmod-usb-net-cdc-mbim umbim picocom \
 kmod-scsi-core kmod-block2mtd fdisk lsblk speedtest-go kmod-tcp-bbr \
 luci-i18n-uhttpd-zh-tw luci-i18n-sqm-zh-tw luci-i18n-cloudflared-zh-tw \
-luci-i18n-acme-zh-tw luci-app-uhttpd luci-app-sqm luci-app-acme kmod-usb-serial-wwan"
+luci-i18n-acme-zh-tw"
 
-PKG="${PKG} ${EXTRA}"
+EXTRA2 = "luci-app-uhttpd luci-app-sqm luci-app-acme kmod-usb-serial-wwan"
+
+PKG="${PKG} ${EXTRA1}" "${EXTRA2}"
+
+[ "${INCLUDE_DOCKER}" = "yes" ] && PKG+=" luci-i18n-dockerman-zh-tw"
+[ "${ENABLE_MODEM_MANAGER}" = "yes" ] && PKG+=" modemmanager luci-proto-modemmanager"
 
 [ "${INCLUDE_DOCKER}" = "yes" ] && PKG+=" luci-i18n-dockerman-zh-tw"
 [ "${ENABLE_MODEM_MANAGER}" = "yes" ] && PKG+=" modemmanager luci-proto-modemmanager"
 
 echo "[`date '+%F %T'`] Packages: ${PKG}"
 
-ls -al /home/build/openwrt
+tree /a /f /home/build/openwrt
+ls -al /home/build
 make -C /home/build/openwrt image \
      PROFILE="generic" \
      PACKAGES="$PKG" \
