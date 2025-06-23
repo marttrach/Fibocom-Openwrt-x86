de49e7122c16730c1fd1905f647e8f2bc18f4cb4
marttracg <martt4946@gmail.com>
2025-06-23 21:43:17 +0800

    [DEV]

--- test/files/etc/uci-defaults/99-custom.sh
+++ test/files/etc/uci-defaults/99-custom.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # 99-custom.sh ── 首次開機自動設定
 
-###############################################################################
-# 0. 動態收集網卡 (eth*/en*)
-###############################################################################
 ports=""
 for iface in /sys/class/net/*; do
     iface_name=$(basename "$iface")
@@ -13,9 +10,6 @@ for iface in /sys/class/net/*; do
 done
 ports=$(echo "$ports" | awk '{$1=$1};1')   # 去掉前導空白
 
-###############################################################################
-# 1. br-lan 介面設定
-###############################################################################
 uci -q delete network.br_lan                   # 清除舊的 device 定義（若存在）
 uci set network.br_lan='device'
 uci set network.br_lan.name='br-lan'
@@ -27,16 +21,10 @@ uci set network.lan.proto='static'
 uci set network.lan.ipaddr='__BRLAN_IP__'
 uci set network.lan.netmask='255.255.255.0'
 
-###############################################################################
-# 2. DHCP 設定
-###############################################################################
 uci set dhcp.lan.start='100'
 uci set dhcp.lan.limit='150'
 uci set dhcp.lan.leasetime='12h'
 
-###############################################################################
-# 3. ModemManager (若啟用)
-###############################################################################
 if [ "__ENABLE_MM__" = "yes" ]; then
     uci batch <<UCI
 set network.cell=interface
@@ -48,9 +36,7 @@ UCI
     [ -n "__PIN__" ] && uci set network.cell.pincode='__PIN__'
 fi
 
-###############################################################################
-# 4. 基本防火牆區 (Docker 用) ── 若你無 Docker 可刪掉此段
-###############################################################################
+
 uci add firewall zone
 uci set firewall.@zone[-1].name='docker'
 uci set firewall.@zone[-1].input='ACCEPT'
@@ -70,16 +56,12 @@ uci add firewall forwarding
 uci set firewall.@forwarding[-1].src='lan'
 uci set firewall.@forwarding[-1].dest='docker'
 
-###############################################################################
-# 5. 改版號 (openwrt_release)
-###############################################################################
+
 FILE_PATH="/etc/openwrt_release"
 NEW_DESCRIPTION="Compiled by MattFlow"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
-###############################################################################
-# 6. 寫入設定並結束
-###############################################################################
+
 uci commit network
 uci commit dhcp
 uci commit firewall
